eb835c05d4689dc626884b8fb4a7dac0424afb88
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2021-10-03 15:11:52 +0000

    Corrigindo rotas de login e logout

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,1 +1,2 @@
-User.create(name: 'Admin', cpf:'11111111111', role: :admin)
+User.create(name: 'Admin', cpf:'11111111111', role: :admin, email:'admin@email.com')
+User.create(name: 'Employee', cpf:'22222222222', role: :employee, email:'employee@email.com')
