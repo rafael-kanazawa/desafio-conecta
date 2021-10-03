347f2866189a221f4d614ede8b9e5f4a80e7ff22
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2021-10-03 14:35:16 +0000

    Corrigindo routes e criando arquivo ability

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :stock_transactions, only: {:index, :show, :delete}
+  resources :stock_transactions, only: [:index, :show, :delete]
   resources :products do 
     resources :stocks, only: :update
   end
