7e6e07cdc407988f9278739d2e97cfb9c0ed2895
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2021-10-04 01:47:52 +0000

    Deploy heroku

---
+++ db/seeds/production.rb
@@ -0,0 +1,2 @@
+User.create!(name: 'Admin', cpf:'11111111111', role: :admin, email:'admin@email.com', password: "123456")
+User.create!(name: 'Employee', cpf:'22222222222', role: :employee, email:'employee@email.com', password: "123456")
