805c65f2e2c71569354e9f906e93a58dea40abdd
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2021-10-04 00:06:46 +0000

    Implementando validações de user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,13 @@ class User < ApplicationRecord
 
   enum role: {employee: 0, admin: 1}
 
+  validates :name, :email,
+    format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" },
+    presence: true
+  
+  validates :cpf,
+    format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" },
+    presence: true,
+    length: { is: 11 }
  
 end
