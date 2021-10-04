682743148db94a778c524ab54a4bfa967accfe99
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2021-10-04 01:29:46 +0000

    Corrigindo user validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,31 @@
 class User < ApplicationRecord
   has_secure_password
+  USER_ROLES = {employee: 0, admin: 1} 
+  enum role: USER_ROLES
 
-  enum role: {employee: 0, admin: 1}
-
-  validates :name, :email,
+  validates :name,
     format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" },
     presence: true
   
+  validates :email, presence: true
+
   validates :cpf,
-    format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" },
     presence: true,
     length: { is: 11 }
+
+  validate do
+    if @not_valid_role
+      errors.add(:role, "Invalid user role value")
+    end
+  end
+
+  def role=(value)
+    value = value.to_s
+    unless USER_ROLES.includes?(value.to_sym)
+      @not_valid_role = true
+    else
+      value
+    end
+  end
  
 end
